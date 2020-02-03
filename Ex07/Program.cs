3a82429525919439a91c654822f7e6617661ab0c
Staicu Adrian <adrianstacu888@gmail.com>
2020-02-03 22:48:46 +0200

    Add project files.

---
+++ Ex07/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numer1 = 21;
+            if (numer1 % 2 == 0)
+            {
+                Console.WriteLine("Numarul este par");
+            } 
+            else
+            {
+                Console.WriteLine("Numarul este impar");
+            }
+        }
+    }
+}
